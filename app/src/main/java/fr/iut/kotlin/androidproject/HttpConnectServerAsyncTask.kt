c485eea543a43b7dcf104187946749aece433884
DUMESTRE LEON p2006532 <leon.dumestre@etu.univ-lyon1.fr>
2022-03-02 17:27:03 +0100

    Ajout du Tp AsyncTask

---
+++ app/src/main/java/fr/iut/kotlin/androidproject/HttpConnectServerAsyncTask.kt
@@ -0,0 +1,40 @@
+package fr.iut.kotlin.androidproject
+
+import android.annotation.SuppressLint
+import android.os.AsyncTask
+import android.util.Log
+import android.widget.TextView
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class HttpConnectServerAsyncTask : AsyncTask<Any, Void, String>() {
+
+    private lateinit var host : String
+    private lateinit var tv : TextView
+
+    override fun doInBackground(vararg params: Any?): String {
+        host = params[0] as String
+        tv = params[1] as TextView
+
+        val url = URL(host)
+        val urlConnection = url.openConnection() as HttpURLConnection
+
+        var text = ""
+        if (urlConnection.responseCode == HttpURLConnection.HTTP_OK) {
+            val input = BufferedReader(InputStreamReader(urlConnection.inputStream))
+            text = input.readLine()
+            Log.d("AsyncTask", "flux = $text")
+            input.close()
+        }
+        urlConnection.disconnect()
+        return text
+    }
+
+    override fun onPostExecute(result: String?) {
+        tv.text = JSONObject(result).getString("datetime");
+
+    }
+}
--- app/src/main/java/fr/iut/kotlin/androidproject/MainActivity.kt
+++ app/src/main/java/fr/iut/kotlin/androidproject/MainActivity.kt
@@ -2,10 +2,62 @@ package fr.iut.kotlin.androidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var URL_OPENDATA = "https://public.opendatasoft.com/explore/dataset/arome-0025-enriched/information/?disjunctive.commune&disjunctive.code_commune"
+    private var URL_TIME_TEXT = "http://worldtimeapi.org/api/timezone/Europe/paris"
+    private var URL_METEO_TEXT = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=arome-0025-enriched&q=&facet=commune&facet=code_commune"
+    private lateinit var tvDate : TextView
+    private lateinit var btDate : Button
+    private lateinit var btHtml : Button
+    private lateinit var btGoogle : Button
+    private lateinit var btMeteo : Button
+    private lateinit var webView : WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btDate = findViewById(R.id.btDate)
+        tvDate = findViewById(R.id.tvDate)
+        btHtml = findViewById(R.id.btTextHtml)
+        btGoogle = findViewById(R.id.btGoogle)
+        btMeteo = findViewById(R.id.btMeteo)
+        webView = findViewById(R.id.webView)
+
+        btDate.setOnClickListener(this)
+        btHtml.setOnClickListener(this)
+        btGoogle.setOnClickListener(this)
+        btMeteo.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btDate -> {
+                HttpConnectServerAsyncTask().execute(URL_TIME_TEXT, tvDate)
+            }
+
+            R.id.btTextHtml -> {
+                val strHtml = "<html><body><strong> <em>Ceci est un texte au format HTML </em> </strong></br>qui s'affiche très simplement</body></html>";
+                webView.loadData(strHtml , "text/html; charset=utf-8", "UTF-8")
+            }
+
+            R.id.btGoogle -> {
+                webView.loadUrl(URL_OPENDATA)
+                webView.webViewClient = WebViewClient()
+            }
+
+            R.id.btMeteo -> {
+                webView.loadUrl(URL_METEO_TEXT)
+            }
+        }
+    }
+
+
 }
