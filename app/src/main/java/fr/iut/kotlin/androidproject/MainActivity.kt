533d0a1bcb9cb6592604145b63b4df783f819319
DUMESTRE LEON p2006532 <leon.dumestre@etu.univ-lyon1.fr>
2022-03-08 17:58:34 +0100

    Ajout get Location

--- app/src/main/java/fr/iut/kotlin/androidproject/MainActivity.kt
+++ app/src/main/java/fr/iut/kotlin/androidproject/MainActivity.kt
@@ -1,14 +1,24 @@
 package fr.iut.kotlin.androidproject
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, LocationListener {
 
     private var URL_OPENDATA = "https://public.opendatasoft.com/explore/dataset/arome-0025-enriched/information/?disjunctive.commune&disjunctive.code_commune"
     private var URL_TIME_TEXT = "http://worldtimeapi.org/api/timezone/Europe/paris"
@@ -19,6 +29,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btGoogle : Button
     private lateinit var btMeteo : Button
     private lateinit var webView : WebView
+    private lateinit var locationManager: LocationManager
+    private val locationPermissionCode = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +66,34 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.btMeteo -> {
-                webView.loadUrl(URL_METEO_TEXT)
+                getLocation()
+            }
+        }
+    }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0F, this)
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        location.accuracy = 1F;
+        tvDate.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+        Toast.makeText(this, "Latitude: " + location.latitude + " , Longitude: " + location.longitude, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
             }
         }
     }
